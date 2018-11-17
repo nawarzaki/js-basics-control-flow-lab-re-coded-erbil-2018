c30d12325b191636c97bcdc6379b774b624e32e2
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-17 15:12:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result =""
+  if (someValue === 199) {
+    result = 'This one is on me!'
+  } else if (someValue === 2001) {
+      result = 'I will gladly take your thirty bucks.'
+    } else if (someValue === 2501) {
+      result = 'No can do.'
+    }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  let ternaryCheckCity = ""
+if(city === 'NYC' ?  true : false) {
+   ternaryCheckCity = 'Ok, sounds good.'
+ } else if(city === 'Pittsburgh' ? true : false){
+   ternaryCheckCity = 'No go.'
+ }
+return ternaryCheckCity
+}
+
+function switchOnCharmFromTip(respond) {
+let tip = ""
+switch (respond) {
+  case 'generous':
+  tip = 'Thank you so much.'
+    break;
+
+    case 'not as generous':
+    tip = 'Thank you.'
+      break;
+      case 'thanks for everything':
+      tip = 'Bye.'
+
+}
+
+}
